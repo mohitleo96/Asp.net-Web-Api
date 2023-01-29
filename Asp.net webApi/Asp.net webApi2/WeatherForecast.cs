73a3222f621e6ee330cfb2fa4070cb359da09415
Mohit Rao <mmohitrrao96@gmail.com>
2023-01-29 13:56:54 +0530

    Second Project

---
+++ Asp.net webApi/Asp.net webApi2/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Asp.net_webApi2
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
