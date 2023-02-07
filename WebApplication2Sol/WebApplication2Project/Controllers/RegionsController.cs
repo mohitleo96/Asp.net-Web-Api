bd3171ca4659cdc411ed7dfe34a23ede131a13d4
Mohit Rao <mmohitrrao96@gmail.com>
2023-02-07 21:08:50 +0530

    Revert "End of Section 4"
    This reverts commit dfd04ad30f54ac2f77aa43226e3b2ccecae0f76a.

--- WebApplication2Sol/WebApplication2Project/Controllers/RegionsController.cs
+++ WebApplication2Sol/WebApplication2Project/Controllers/RegionsController.cs
@@ -1,7 +1,6 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using WebApplication2Project.Models.domain;
-using WebApplication2Project.Models.DTO;
 using WebApplication2Project.Repository;
 
 namespace WebApplication2Project.Controllers
@@ -28,9 +27,7 @@ namespace WebApplication2Project.Controllers
 
         [HttpGet]
         //So now it get all the data from the database.
-        public async Task<IActionResult> GetAllRegionsAsync()
-
-
+        public async Task<IActionResult> GetAllRegions()
         {
             var regions = await regionRepository.GetAllAsync();
 
@@ -57,128 +54,5 @@ namespace WebApplication2Project.Controllers
             var regionsDTO=mapper.Map<List<Models.DTO.RegionDTO>>(regions);
             return Ok(regionsDTO);
         }
-
-
-        [HttpGet]
-        //Route helps to insert only Guid value in  id.
-        [Route("{id:guid}")]
-        // So we need to get an action name in return CreatedAtAction()
-        [ActionName("GetRegionAsync")]
-        //So now it get the data based on id from the database.
-        public async Task<IActionResult> GetRegionAsync(Guid id)
-        {
-           var region = await regionRepository.GetAsync(id);
-
-            //Here we use store value in AutoMapper so it matches with the DTOfields.
-            //Automapper also helps to reduce code beacause we written only one time and use multiple times.
-             
-           var regionDTO1= mapper.Map<Models.DTO.RegionDTO>(region);
-            return Ok(regionDTO1);
-        }
-      
-        //So use HTTPPost because we add some data into server.
-        [HttpPost]
-        public async Task<IActionResult> AddRegionAsync(Models.DTO.AddRegionRequest addRegionRequest)
-        {
-            //we do manually without AutoMapper
-            //First Convert addRegionRequest to DomainModel
-            var region = new Models.domain.Region()
-            {
-                Code = addRegionRequest.Region_Code,
-                Name = addRegionRequest.FullName,
-                Lat = addRegionRequest.Latitute,
-                Long = addRegionRequest.Longitude,
-                Area = addRegionRequest.LocalArea,
-                Population = addRegionRequest.Overall_Population,
-
-            };
-
-            //Pass Details to repository
-           region = await regionRepository.AddAsync(region);
-            //if null then Not Found
-            if (region == null)
-            {
-                return NotFound();
-            }
-
-            //Convert back to DTO
-            var regionDTO2 = new Models.DTO.RegionDTO()
-            {
-                Id = region.Id,
-                Region_Code = region.Code,
-                FullName = region.Name,
-                LocalArea = region.Area,
-                Latitute = region.Lat,
-                Longitude = region.Long,
-                Overall_Population = region.Population,
-            };
-
-            return CreatedAtAction(nameof(GetRegionAsync), new { id = regionDTO2.Id }, regionDTO2);
-        }
-        
-        
-        [HttpDelete]
-        //Route helps to insert only Guid value in  id.
-        [Route("{id:guid}")]
-        public async Task<IActionResult> DeleteRegion(Guid id)
-        {
-            //Get region from Database
-            var region = await regionRepository.DeleteAsync(id);
-            //if null notfound
-            if(region==null)
-            {
-                return NotFound();
-            }
-            //convert response back to DTO
-            var regionDTO3 = new Models.DTO.RegionDTO()
-            {
-                Id = region.Id,
-                Region_Code = region.Code,
-                FullName = region.Name,
-                LocalArea = region.Area,
-                Latitute = region.Lat,
-                Longitude = region.Long,
-                Overall_Population = region.Population,
-            };
-            //Return OK response
-            return Ok(regionDTO3); 
-        }
-
-        [HttpPut]
-        [Route("{id:guid}")]
-        public async Task<IActionResult> UpdateRegionAsync([FromRoute]Guid id, [FromBody]Models.DTO.UpdateRegionRequest updateRegionRequest)
-        {
-            //Convert DTO to Domain
-            var region = new Models.domain.Region()
-            {
-                Code = updateRegionRequest.Region_Code,
-                Name =updateRegionRequest.FullName,
-                Lat = updateRegionRequest.Latitute,
-                Long =updateRegionRequest.Longitude,
-                Area = updateRegionRequest.LocalArea,
-                Population = updateRegionRequest.Overall_Population,
-            };
-            //Update region using Repository
-            region = await regionRepository.UpdateAsync(id, region);
-            //if null then Not Found
-            if (region==null)
-            {
-                return NotFound();
-            }
-            //Convert Domain back to DTO
-            var regionDTO4 = new Models.DTO.RegionDTO
-            {
-                Id = region.Id,
-                Region_Code = region.Code,
-                FullName = region.Name,
-                LocalArea = region.Area,
-                Latitute = region.Lat,
-                Longitude = region.Long,
-                Overall_Population = region.Population,
-            };
-            //Return Ok response
-            return Ok(regionDTO4);
-        }
     }
-
 }
--- WebApplication2Sol/WebApplication2Project/Repository/RegionRepository.cs
+++ WebApplication2Sol/WebApplication2Project/Repository/RegionRepository.cs
@@ -16,56 +16,9 @@ namespace WebApplication2Project.Repository
             //to assign a private readonly field
             this.nZWalksDBContext= nZWalksDBContext;
         }
-        public async Task<Region> AddAsync(Region region)
-        {
-             region.Id= Guid.NewGuid();
-            await nZWalksDBContext.AddAsync(region);
-            await nZWalksDBContext.SaveChangesAsync();
-            return region;
-        }
-
-        public async Task<Region> DeleteAsync(Guid id)
-        {
-            //to search the id data is available or not.
-            var region = await nZWalksDBContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
-            if (region==null)
-            {
-                return null;
-            }
-            //delete the region
-            nZWalksDBContext.Regions.Remove(region);
-            await nZWalksDBContext.SaveChangesAsync();
-            return region;
-        }
-
         public async Task<IEnumerable<Region>> GetAllAsync()
         {
             return await nZWalksDBContext.Regions.ToListAsync();
-        }
-        public async Task<Region> GetAsync(Guid id)
-        {
-            //firstoedefault helps to get the ID if id matches is return
-            //the value if not it return null value.
-            return await nZWalksDBContext.Regions.FirstOrDefaultAsync(x=>x.Id==id);
-        }
-
-        public async Task<Region> UpdateAsync(Guid id, Region region)
-        {
-           var existingRegion = await nZWalksDBContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
-
-            if(existingRegion==null)
-            {
-                return null;
-            }
-            existingRegion.Code = region.Code;
-            existingRegion.Name = region.Name;
-            existingRegion.Area = region.Area;
-            existingRegion.Lat= region.Lat;
-            existingRegion.Long= region.Long;
-            existingRegion.Population= region.Population;
-
-            await nZWalksDBContext.SaveChangesAsync();
-            return existingRegion;
         }
     }
 }
