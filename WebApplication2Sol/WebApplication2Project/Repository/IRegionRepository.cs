dfd04ad30f54ac2f77aa43226e3b2ccecae0f76a
Mohit Rao <mmohitrrao96@gmail.com>
2023-02-07 21:07:54 +0530

    End of Section 4

--- WebApplication2Sol/WebApplication2Project/Repository/IRegionRepository.cs
+++ WebApplication2Sol/WebApplication2Project/Repository/IRegionRepository.cs
@@ -5,5 +5,18 @@ namespace WebApplication2Project.Repository
     public interface IRegionRepository
     {
        Task<IEnumerable<Region>> GetAllAsync();
+
+        //This GetAsync will get a single region
+        //and we will get the single region based on the ID of the region.
+        Task<Region> GetAsync(Guid id);
+
+        //adding new data
+        Task<Region> AddAsync(Region region);  
+
+        //daleting the data
+        Task<Region> DeleteAsync(Guid id);
+
+        //Updating the Data
+        Task<Region> UpdateAsync(Guid id, Region region);
     }
 }
